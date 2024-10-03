b6f4a2da7864e77337eb8b9829e058230d8c0a75
stio314 <squartio314@gmail.com>
2024-10-03 16:42:02 -0700

    Update post.js

--- post.js
+++ post.js
@@ -1,4 +1,6 @@
-document.querySelector('#submit_question').addEventListener('click', () => {
+const url = 'https://stio-webhook-server.onrender.com'
+
+document.querySelector('#submit_question').addEventListener('click', () => {    
     const question = document.querySelector('#question').value
 
     if (!question) {
@@ -11,12 +13,12 @@ document.querySelector('#submit_question').addEventListener('click', () => {
         const data = {'question': question}
         const req = new XMLHttpRequest()
 
-        req.open('POST', 'https://stio-webhook-server.onrender.com/send_question')
+        req.open('POST', `${url}/send_question`)
         req.setRequestHeader('Content-Type', 'application/json')
 
         req.onreadystatechange = () => {
             if (req.readyState === 4) {
-                if (req.status === 0 || (req.status >= 200 && req.status < 400)) {
+                if ((req.status === 0 || (req.status >= 200 && req.status < 400)) && req.response !== '') {
                     document.querySelector('#submit_question').innerText = 'thankks'
                 } else {
                     document.querySelector('#submit_question').removeAttribute('disabled')
@@ -40,21 +42,21 @@ document.querySelector('#submit_art').addEventListener('click', () => {
     const data = {'image': image}
     const req = new XMLHttpRequest()
 
-    req.open('POST', 'https://stio-webhook-server.onrender.com/send_art')
+    req.open('POST', `${url}/send_art`)
     req.setRequestHeader('Content-Type', 'application/json')
 
     req.onreadystatechange = () => {
-            if (req.readyState === 4) {
-                if (req.status === 0 || (req.status >= 200 && req.status < 400)) {
-                    document.querySelector('#submit_art').innerText = 'thankks'
-                } else {
-                    document.querySelector('#submit_art').removeAttribute('disabled')
-                    document.querySelector('#submit_art').innerText = 'failed to submit'
-                }
-
-                console.log(req.response)
+        if (req.readyState === 4) {
+            if ((req.status === 0 || (req.status >= 200 && req.status < 400)) && req.response !== '') {
+                document.querySelector('#submit_art').innerText = 'thankks'
+            } else {
+                document.querySelector('#submit_art').removeAttribute('disabled')
+                document.querySelector('#submit_art').innerText = 'failed to submit'
             }
+
+            console.log(req.response)
         }
+    }
 
     req.send(JSON.stringify(data))
 })
