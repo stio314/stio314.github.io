aa31387f7069dbaa8927e8770e008f81db54fa7f
stio314 <squartio314@gmail.com>
2024-10-03 19:00:05 -0700

    Update post.js

--- post.js
+++ post.js
@@ -1,28 +1,40 @@
 const url = 'https://stio-webhook-server.onrender.com'
 
-document.querySelector('#submit_question').addEventListener('click', () => {    
-    const question = document.querySelector('#question').value
+document.querySelector('#submit').addEventListener('click', () => {
+    const imageData = document.querySelector('#canvas_static').getContext('2d').getImageData(0, 0, 600, 400).data
+    let empty = true
 
-    if (!question) {
-        alert('please type something in the box thanks')
-        return
+    for (let i=0; i<imageData.length; i+=4) {
+        if (imageData[i] !== 255 || imageData[i+1] !== 255 || imageData[i+2] !== 255) {
+            empty = false
+            break
+        }
+    }
+
+    if ( empty ) {
+        return alert('cannot send empty canvas')
     } else {
-        document.querySelector('#submit_question').setAttribute('disabled', 'true')
-        document.querySelector('#submit_question').innerText = 'sending... (may take a while i have no money)'
+        const question = document.querySelector('#question').value
+        const image = document.querySelector('#canvas_static').toDataURL('image/png').split(',')[1]
+        
+        document.querySelector('#submit').setAttribute('disabled', 'true')
+        document.querySelector('#submit').innerText = 'sending... (may take a while i have no money)'
 
-        const data = {'question': question}
+        const data = {'image': image}
+        if (question !== '') data.question = question    
+
         const req = new XMLHttpRequest()
 
-        req.open('POST', `${url}/send_question`)
+        req.open('POST', `${url}/send`)
         req.setRequestHeader('Content-Type', 'application/json')
 
         req.onreadystatechange = () => {
             if (req.readyState === 4) {
                 if ((req.status === 0 || (req.status >= 200 && req.status < 400)) && req.response !== '') {
-                    document.querySelector('#submit_question').innerText = 'thankks'
+                    document.querySelector('#submit').innerText = 'thankks'
                 } else {
-                    document.querySelector('#submit_question').removeAttribute('disabled')
-                    document.querySelector('#submit_question').innerText = 'failed to submit'
+                    document.querySelector('#submit').removeAttribute('disabled')
+                    document.querySelector('#submit').innerText = 'failed to submit'
                 }
 
                 console.log(req.response)
@@ -31,32 +43,4 @@ document.querySelector('#submit_question').addEventListener('click', () => {
 
         req.send(JSON.stringify(data))
     }
-})
-
-document.querySelector('#submit_art').addEventListener('click', () => {
-    const image = document.querySelector('#canvas_static').toDataURL('image/png').split(',')[1]
-
-    document.querySelector('#submit_art').setAttribute('disabled', 'true')
-    document.querySelector('#submit_art').innerText = 'sending... (may take a while i have no money)'
-
-    const data = {'image': image}
-    const req = new XMLHttpRequest()
-
-    req.open('POST', `${url}/send_art`)
-    req.setRequestHeader('Content-Type', 'application/json')
-
-    req.onreadystatechange = () => {
-        if (req.readyState === 4) {
-            if ((req.status === 0 || (req.status >= 200 && req.status < 400)) && req.response !== '') {
-                document.querySelector('#submit_art').innerText = 'thankks'
-            } else {
-                document.querySelector('#submit_art').removeAttribute('disabled')
-                document.querySelector('#submit_art').innerText = 'failed to submit'
-            }
-
-            console.log(req.response)
-        }
-    }
-
-    req.send(JSON.stringify(data))
 })
